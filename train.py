05c7d65edc7e724bf38b344cfd9d3f67f4ef8819
samerbahri <sameur.bahri@gmail.com>
2019-02-15 21:22:52 +0100

    updating the train issue #1

--- train.py
+++ train.py
@@ -1,48 +1,55 @@
-'''
-
-BME AI - Speech Emotion Analysis - Feature Extraction
------------------------------------------------------
-This script will automatically train and test the model
-
-********* Instructions ************
-
-
-***********************************
-
-RAVDESS filename identifiers:
-
-  Modality (01 = full-AV, 02 = video-only, 03 = audio-only).
-  Vocal channel (01 = speech, 02 = song).
-  Emotion (01 = neutral, 02 = calm, 03 = happy, 04 = sad, 05 = angry, 06 = fearful, 07 = disgust, 08 = surprised).
-  Emotional intensity (01 = normal, 02 = strong). NOTE: There is no strong intensity for the 'neutral' emotion.
-  Statement (01 = 'Kids are talking by the door', 02 = 'Dogs are sitting by the door').
-  Repetition (01 = 1st repetition, 02 = 2nd repetition).
-  Actor (01 to 24. Odd numbered actors are male, even numbered actors are female).
-
-'''
-
-# Import libraries.
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 import pyarrow.parquet as pq
 from sklearn.utils import shuffle
 from sklearn.preprocessing import LabelEncoder
 import time
 
-print(' ___ __  __ ___     _   ___ ')
-print('| _ )  \/  | __|   /_\ |_ _|')
-print('| _ \ |\/| | _|   / _ \ | | ')
-print('|___/_|  |_|___| /_/ \_\___|')
-print('                            ')
+data = pq.read_pandas('audio-features.parquet').to_pandas()
+data = shuffle(data)
+print(data.columns)
 
-# Time script execution for performance evaluation.
-start = time.time()
+# https://stackoverflow.com/questions/24147278/how-do-i-create-test-and-train-samples-from-one-dataframe-with-pandas
+msk = np.random.rand(len(data)) < 0.8
 
-Hdata=pq.read_pandas('audio-features.parquet').to_pandas()
-Hdata=shuffle(Hdata)
-i = np.random.rand(len(Hdata)) < 0.8
-train = Hdata[i]
-test = Hdata[~i]
+train_data = data[msk]
+test_data = data[~msk]
+
+train_features = train_data['features'].values
+train_emotions = train_data['emotion'].values
+test_features = test_data['features'].values
+test_emotions = test_data['emotion'].values
+
+#print(train_features[0])
+
+'''
+model = tf.keras.Sequential()
+
+model.add(Conv1D(256, 5,padding='same',
+                 input_shape=(216,1)))
+model.add(Activation('relu'))
+model.add(Conv1D(128, 5,padding='same'))
+model.add(Activation('relu'))
+model.add(Dropout(0.1))
+model.add(MaxPooling1D(pool_size=(8)))
+model.add(Conv1D(128, 5,padding='same',))
+model.add(Activation('relu'))
+#model.add(Conv1D(128, 5,padding='same',))
+#model.add(Activation('relu'))
+#model.add(Conv1D(128, 5,padding='same',))
+#model.add(Activation('relu'))
+#model.add(Dropout(0.2))
+model.add(Conv1D(128, 5,padding='same',))
+model.add(Activation('relu'))
+model.add(Flatten())
+model.add(Dense(10))
+model.add(Activation('softmax'))
+opt = keras.optimizers.rmsprop(lr=0.00001, decay=1e-6)<
+
+
+
+
 
 trainfeatures = train.iloc[:,[-1]]
 trainlabel = train.iloc[:,[-2]]
@@ -58,12 +65,12 @@ y_test = np.array(testlabel)
 x_traincnn =np.expand_dims(X_train, axis=2)
 x_testcnn= np.expand_dims(X_test, axis=2)
 
-'''
 y_train = np_utils.to_categorical(lb.fit_transform(y_train))
 y_test = np_utils.to_categorical(lb.fit_transform(y_test))
-'''
+
 end = time.time()
 print('Data Division is done')
 print(len(train.index), 'training sample')
 print(len(test.index), 'testing sample')
 print('This script took ', str(round(end - start, 2)), 'seconds to execute.')
+'''
